fa147a6bceb4fd033a891b1d334b3db2ffaceb67
Namyejun <yejun1148@gmail.com>
2023-09-22 20:20:07 +0900

    add question controller

--- src/main/java/com/yejun/app/controller/QuestionController.java
+++ src/main/java/com/yejun/app/controller/QuestionController.java
@@ -1,8 +1,44 @@
 package com.yejun.app.controller;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.yejun.app.domain.Question;
+import com.yejun.app.dto.QuestionDTO;
+import com.yejun.app.dto.ResponseDTO;
+import com.yejun.app.service.QuestionService;
+
+import jakarta.validation.Valid;
 
 @Controller
+@RequestMapping(value = "/question")
 public class QuestionController {
+	
+	@Autowired
+	private ModelMapper modelMapper;
+	
+	@Autowired
+	private QuestionService questionService;
+	
+	public @ResponseBody ResponseDTO<?> insertQuestion(@Valid @RequestBody QuestionDTO questionDTO, BindingResult bindingResult) {
+		Question question = modelMapper.map(questionDTO, Question.class);
+		questionService.insertQuestion(question);
+		return new ResponseDTO<>(HttpStatus.OK.value(), "문항 입력 완료");
+	}
+	
+	@PutMapping("/updateQuestion")
+	public @ResponseBody ResponseDTO<?> updateQuestion(@Valid @RequestBody QuestionDTO questionDTO, BindingResult bindingResult) {
+		 Question question = modelMapper.map(questionDTO, Question.class);
+		 questionService.updateQuestion(question);
+		 return new ResponseDTO<>(HttpStatus.OK.value(), "문항 업데이트 완료");
+	}
+	
 	
 }
--- src/main/java/com/yejun/app/service/QuestionService.java
+++ src/main/java/com/yejun/app/service/QuestionService.java
@@ -1,5 +1,7 @@
 package com.yejun.app.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,13 +20,21 @@ public class QuestionService {
 		questionRepository.save(question);
 	}
 	
+	@Transactional
 	public void updateQuestion(Question question) {
 		Question findQuestion = questionRepository.findById(question.getId()).get();
 		findQuestion.setQuestion(question.getQuestion());
 	}
 	
+	@Transactional
 	public void updateAnswer(Question question) {
 		Question findQuestion = questionRepository.findById(question.getId()).get();
 		findQuestion.setAnswer(question.getAnswer());
+	}
+	
+	@Transactional(readOnly = true)
+	public List<Question> getQuestionListByKeyword(String keyword) {
+		List<Question> questionList = questionRepository.findByQuestionOrAnswerLike(keyword);
+		return questionList;
 	}
 }
